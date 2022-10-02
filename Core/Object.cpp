048425b3c8c8cfcbf51a3d050fdd181b484b55c7
Anilforextra <anilforextra@gmail.com>
2022-10-02 11:17:01 +0545

    Add Object class which is the implementation of the Observer pattern.

---
+++ Core/Object.cpp
@@ -0,0 +1,40 @@
+#include "Object.hpp"
+
+void Subject::notify_all() {
+  for (auto &fn_ptr : observers) {
+    if (fn_ptr) {
+      fn_ptr();
+    }
+  }
+}
+
+void Object::connect(std::string message, std::function<void()> fn) {
+  // If there's existing message, then append the new fn ptr as the observer
+  // for the given message.
+  for (auto &signal : signals) {
+    if (signal.message == message) {
+      std::cout << "\"" << message
+                << "\" Found. Binding a new observer to it.\n";
+      signal.add_observer(fn);
+      return;
+    }
+  }
+
+  // If no existing messages,then append one.
+  Subject new_signal;
+  new_signal.message = message;
+  new_signal.add_observer(fn);
+
+  signals.push_back(new_signal);
+  std::cout << "Adding new Signal with message: \"" << message << "\"\n";
+}
+
+void Object::emit_signal(std::string emitted_message) {
+  std::cout << "Message Broadcasted: " << emitted_message << "\n";
+  for (auto &signal : signals) {
+    if (signal.message == emitted_message) {
+      signal.notify_all();
+      return;
+    }
+  }
+}
---
+++ Core/Object.hpp
@@ -0,0 +1,52 @@
+#ifndef OBJECT_HPP
+#define OBJECT_HPP
+
+#include <functional>
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Subject {
+public:
+  std::string message;
+  std::vector<std::function<void()>> observers;
+
+  void add_observer(std::function<void()> fn) { observers.push_back(fn); }
+  void notify_all();
+};
+
+class Object {
+public:
+  std::vector<Subject> signals;
+
+  void connect(std::string message, std::function<void()> fn);
+  void emit_signal(std::string emitted_message);
+};
+
+/*
+Usage:
+
+class Button : public Object {
+public:
+  void pressed() {
+    emit_signal("Pressed");
+    std::cout << "Button Pressed.\n";
+  }
+};
+
+int main() {
+  Button btn;
+
+  auto test = []() { std::cout << "Hi from the message receiver.\n"; };
+  auto test2 = []() { std::cout << "Hi from the other message receiver.\n"; };
+
+  btn.connect("Pressed", test);
+  btn.connect("Pressed", test2);
+
+  btn.pressed();
+
+  return 0;
+}
+*/
+
+#endif // OBJECT_HPP
